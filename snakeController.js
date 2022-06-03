76b39a2b734a5f5a50cf9918d283a462df337863
kaanfkennedy <dev.kaanfkennedy@gmail.com>
2022-06-03 18:50:14 +0300

    add snakeController

---
+++ snakeController.js
@@ -0,0 +1,30 @@
+let inputDirection = { x: 0, y: 0};
+let lastInputDirection = { x: 0, y: 0};
+
+window.addEventListener('keydown', (e) => {
+    // console.log(e.key)
+    switch(e.key) {
+        case 'ArrowUp':
+            inputDirection = { x: 0, y: -1 };
+            break;
+
+        case 'ArrowDown': 
+            inputDirection = { x: 0, y: 1 };
+            break;
+        
+        case 'ArrowLeft': 
+            inputDirection = { x: -1, y:0 };
+            break;
+            
+        case 'ArrowRight': 
+            inputDirection = { x: 1, y: 0 };
+            break;
+    }
+});
+
+const getLastInputDirection = () => {
+    lastInputDirection = inputDirection;
+    return lastInputDirection;
+}
+
+export { getLastInputDirection }
